3ba32ae11d420bc8c38e9c49d27512ea8a330086
2023-source <122154212+Priyansh218@users.noreply.github.com>
2024-09-07 10:55:32 +0530

    small change

--- facebook-graph-api-frontend/src/pages/Login.js
+++ facebook-graph-api-frontend/src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Login = () => {
   const handleLogin = () => {
-    window.location.href = `https://www.facebook.com/v20.0/dialog/oauth?client_id=${process.env.REACT_APP_FACEBOOK_APP_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=pages_read_engagement,pages_read_user_content,pages_show_list,public_profile,pages_manage_metadata,pages_manage_engagement`;
+    window.location.href = `https://www.facebook.com/v20.0/dialog/oauth?client_id=${process.env.REACT_APP_FACEBOOK_APP_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=pages_read_engagement,read_insights,pages_read_user_content,pages_show_list,public_profile,pages_manage_metadata,pages_manage_engagement`;
     // window.location.href = `https://www.facebook.com/v20.0/dialog/oauth?client_id=${process.env.REACT_APP_FACEBOOK_APP_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=public_profile,pages_show_list,pages_read_engagement`;
 
   };
